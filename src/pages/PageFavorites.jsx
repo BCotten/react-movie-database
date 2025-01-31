0b25f34eb2fa0ef47ae39ad0638649d98274da00
Brandon Cotten <brandon__cotten@hotmail.com>
2025-01-30 17:33:41 -0800

    created inital Pages, about, home, details, notfound, components, foooter, nav, the set up routing

---
+++ src/pages/PageFavorites.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageMovieDetails() {
+  return (
+    <main>
+      <h1>Favorites</h1>
+    </main>
+  );
+}
---
+++ src/pages/PageHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageHome() {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+}
