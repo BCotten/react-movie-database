ac1208ddc969115f96f44a9dc192c065e450c87e
Carlo dela Fuente <gdelafuente1@my.bcit.ca>
2025-02-15 12:46:50 -0800

    fixed typos

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -5,9 +5,9 @@ export default function PageAbout() {
     <main className="text-(--color-neutral-light)">
     <section>
       <h1>About MovieFix</h1>
-      <p>Welcome to MovieFix, your ultimate destination for all things movies! whether you're searching for the latest blockbusters, timeless classics, or hidden gems, MovieFix has you covered. Explore detailed reviews, cast and crew insights, trailers, and personalized reocmmendations tailored to your taste.</p>
+      <p>Welcome to MovieFix, your ultimate destination for all things movies! whether you're searching for the latest blockbusters, timeless classics, or hidden gems, MovieFix has you covered. Explore detailed reviews, cast and crew insights, trailers, and personalized recommendations tailored to your taste.</p>
       <p>
-        Our Mission is to help you discover your next favorite film, on fix at a time.
+        Our Mission is to help you discover your next favorite film, one fix at a time.
       </p>
       <p>Dive in, and let your cinematic journey begin!</p>
     </section>
