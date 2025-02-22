1986af39cddcf7a29fe763db15225a2c047dea69
Nicole <nicolelogan@hotmail.com>
2025-02-22 15:29:23 -0800

    updated hover card
    Adjusted text sizes and character count so there is no overflow of icons

--- src/utilities/changeMaxOverviewLength.js
+++ src/utilities/changeMaxOverviewLength.js
@@ -2,8 +2,8 @@ export default function changeMaxOverviewLength(overview) {
   if (overview == null) {
     return 'N/A';
   }
-  if (overview.length > 250) {
-    return `${overview.slice(0, 250)}...`;
+  if (overview.length > 200) {
+    return `${overview.slice(0, 200)}...`;
   }
   return overview;
 }
